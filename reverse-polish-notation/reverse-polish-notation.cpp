b579409ddb720a0869a53f2ad3bfe775b271dd26
gschick3 <gschick3@gmail.com>
2021-11-21 13:52:08 -0600

    Add project files.

---
+++ reverse-polish-notation/reverse-polish-notation.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
+/*
+Reverse polish notation
+
+v.push_back(x)
+
+v.back() // last element of vector
+
+v.pop_back() // remove last element of vector
+
+if input is one of [*, /, +, -]
+	perform operation
+else
+	add to stack as number
+*/
